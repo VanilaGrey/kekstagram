08d08d5e515638ff625ced3cbf4ee1a9d1049092
VanilaGrey <Allimp_94@mail.ru>
2024-03-14 18:09:48 +0300

    добавление эфффектов

--- js/user-form.js
+++ js/user-form.js
@@ -1,8 +1,4 @@
 // 2.2. Наложение эффекта на изображение:
-// По умолчанию должен быть выбран эффект «Оригинал».
-// На изображение может накладываться только один эффект.
-// При смене эффекта, выбором одного из значений среди радиокнопок .effects__radio, добавить картинке внутри .img-upload__preview CSS-класс,
-// соответствующий эффекту. Например, если выбран эффект .effect-chrome, изображению нужно добавить класс effects__preview--chrome.
 // Интенсивность эффекта регулируется перемещением ползунка в слайдере. Слайдер реалиDзуется сторонней библиотекой для реализации слайдеров noUiSlider.
 //Уровень эффекта записывается в поле .effect-level__value. При изменении уровня интенсивности эффекта (предоставляется API слайдера),
 //CSS-стили картинки внутри .img-upload__preview обновляются следующим образом:
@@ -18,27 +14,21 @@
 // На расширенном тарифе регулировку интенсивности эффекта можно не делать, достаточно применить к изображению только эффект. Слайдер при этом нужно скрыть,
 // библиотеку noUiSlider подключать к странице не нужно.
 
-import { openBigPicture, closeBigPicture } from "./util.js";
-import { EFFECTS } from "./const.js";
 import { checkStringLength } from "./util.js";
 
 const formElement = document.querySelector(".img-upload__form");
 const uploadElement = formElement.querySelector("#upload-file");
 const overlayElement = formElement.querySelector(".img-upload__overlay");
-const controlSmallerElement = formElement.querySelector(
-  ".scale__control--smaller"
-);
-const controlBiggerElement = formElement.querySelector(
-  ".scale__control--bigger"
-);
+const controlSmallerElement = formElement.querySelector(".scale__control--smaller");
+const controlBiggerElement = formElement.querySelector(".scale__control--bigger");
 const controlValueElement = formElement.querySelector("[name=scale]");
 const cancelElement = formElement.querySelector("#upload-cancel");
 const previewElement = formElement.querySelector(".img-upload__preview img");
-const effectsElement = formElement.querySelector(".img-upload__effects");
-const radioElement = formElement.querySelector('[name="effect]');
+const radioElement = formElement.querySelectorAll(".effects__radio");
 const hashtagsElement = formElement.querySelector(".text__hashtags");
 const submitElement = formElement.querySelector(".img-upload__submit");
 const descriptionElement = formElement.querySelector(".text__description");
+const textElement = formElement.querySelector(".text");
 
 let scaleValue = 100;
 const STEP_CONTROL = 25;
@@ -46,15 +36,14 @@ const MAX_HASHTAGS = 5; //колличество хэштегов
 const MAX_HASHTAG_SIZE = 20; // макс длина хэштега
 
 const FIELDS = ["input", "textarea"];
-const RE = /^#[A-Za-zА-Яа-яЁё0-9]{1-19}$/i; //выражение хэштега
+const RE = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/i; //выражение хэштега
 
 const pristine = new Pristine(formElement, {
-  classTo: "text",
-  errorTextParent: "text",
+  classTo: "img-upload__text",
+  errorTextParent: "img-upload__text",
 });
 
 // открытие формы
-
 uploadElement.addEventListener("change", () => {
   let [file] = uploadElement.files; // Получаем выбранный файл
   previewElement.src = URL.createObjectURL(file);
@@ -64,7 +53,6 @@ uploadElement.addEventListener("change", () => {
 });
 
 // закрытие формы
-
 function closeForm() {
   cancelElement.addEventListener("click", () => {
     overlayElement.classList.add("hidden");
@@ -88,6 +76,22 @@ function onUserModalEscKeydown(evt) {
 
 cancelElement.addEventListener("click", () => {
   closeForm();
+});
+
+//изменение эффекта фотографии
+const effectClasses = ["chrome", "sepia", "marvin", "phobos", "heat"];
+
+previewElement.classList.add(".effects__preview--none");
+
+radioElement.forEach((radioButton) => {
+  radioButton.addEventListener("click", (radio) => {
+    const selectedEffect = radio.target.value;
+
+    effectClasses.forEach((effectClass) => {
+      previewElement.classList.remove(`effects__preview--${effectClass}`);
+    });
+    previewElement.classList.add(`effects__preview--${selectedEffect}`);
+  });
 });
 
 // изменение размера загружаемой картинки
@@ -128,27 +132,7 @@ const validateHashtagsHashable = (value) => {
   return hashtags.every((tag) => tag.startsWith("#"));
 };
 
-pristine.addValidator(
-  hashtagsElement,
-  validateHashtagsHashable,
-  "Хэштег начинается с #.",
-  1002,
-  true
-);
-
-// валидация формата тега
-const validateHashtagsFormat = (value) => {
-  const hashtags = value.split(" ");
-  return hashtags.every((tag) => RE.test(tag));
-};
-
-pristine.addValidator(
-  hashtagsElement,
-  validateHashtagsFormat,
-  "Хэштег состоит из букв или цифр.",
-  1000,
-  true
-);
+pristine.addValidator(hashtagsElement, validateHashtagsHashable, "Хэштег начинается с #.", 1002, true);
 
 // валидация максимального кол-ва символов в теге
 const validateHashtagsSizes = (value) => {
@@ -156,13 +140,17 @@ const validateHashtagsSizes = (value) => {
   return hashtags.every((tag) => tag.length <= MAX_HASHTAG_SIZE);
 };
 
-pristine.addValidator(
-  hashtagsElement,
-  validateHashtagsSizes,
-  "Длина хэштега не более 20 символов.",
-  1001
-);
+pristine.addValidator(hashtagsElement, validateHashtagsSizes, "Длина хэштега не более 20 символов.", 1001, true);
 
+// валидация формата тега
+const validateHashtagsFormat = (value) => {
+  const hashtags = value.split(" ");
+  return hashtags.every((tag) => RE.test(tag));
+};
+
+pristine.addValidator(hashtagsElement, validateHashtagsFormat, "Хэштег состоит из букв или цифр.", 1000);
+
+// валидация уникальности тега
 const validateHashtagsUnique = (value) => {
   const hashtags = value.split(" ");
   const uniqueHashtags = new Set(hashtags);
@@ -174,24 +162,16 @@ const validateHashtagsUnique = (value) => {
   return true;
 };
 
-pristine.addValidator(
-  hashtagsElement,
-  validateHashtagsUnique,
-  "Хэштеги должны быть уникальными",
-  1004
-);
+pristine.addValidator(hashtagsElement, validateHashtagsUnique, "Хэштеги должны быть уникальными", 1004);
 
+// валидация длины комментария
 const validateDescription = () => {
   return checkStringLength(descriptionElement.value, 140);
 };
 
-pristine.addValidator(
-  descriptionElement,
-  validateDescription,
-  "Максимальная длина комментария 140 символов",
-  1007
-);
+pristine.addValidator(descriptionElement, validateDescription, "Максимальная длина комментария 140 символов", 1007);
 
+//отправка формы
 submitElement.addEventListener("click", (evt) => {
   evt.preventDefault();
 
@@ -199,5 +179,5 @@ submitElement.addEventListener("click", (evt) => {
   if (!isValid) {
     return;
   }
-  submitElement.submit();
+  formElement.submit();
 });
